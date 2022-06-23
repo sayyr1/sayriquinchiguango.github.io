755d293b07544ca86aac7aebe37972509a027f28
sayyr1 <65569963+sayyr1@users.noreply.github.com>
2022-06-23 13:15:19 -0500

    Add files via upload

---
+++ images/percentage.js
@@ -0,0 +1,18 @@
+let circleElem = document.querySelector(".progress circle");
+let innerText = document.querySelector(".inner-text");
+
+const r = parseInt(circleElem.getAttribute("r"));
+const c = Math.PI*(r*2);
+
+// Target percent here, this is 85
+const toPercent = 85
+
+const dashOffset = ((100-toPercent)/100) * c;
+
+
+innerText.innerText = toPercent + "%";
+
+setTimeout(() => {
+  circleElem.style.strokeDashoffset = dashOffset;
+  
+}, 300);
